7537f14c6fae6fbfb0609f2fbd347f4a242c49e0
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-17 14:30:52 -0500

    started route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongojs = require("mongojs");
+// const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +13,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true
 });
 
 app.use(require("./routes/api.js"));
