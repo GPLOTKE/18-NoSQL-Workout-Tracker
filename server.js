b3b21d9a982e0691adec4c1580c16be0662a597e
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-17 14:12:13 -0500

    const port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongojs = require("mongojs");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 
 
 app.listen(3000, () => {
-    console.log("App running on port 3000!");
+    console.log(`App running on port ${PORT}!`);
 });
