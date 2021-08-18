9f92b657c81cfdbe3a663d5a7756a2af37252fe4
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-18 16:35:17 -0500

    update workout route

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -24,4 +24,16 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body }
+    }, { upsert: true, new: true, setDefaultsOnInsert: true }).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
+
 module.exports = router;
