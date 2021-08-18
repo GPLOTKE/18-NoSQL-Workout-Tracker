78419e5bba21375107fdb2ba6f7e94459b8fcccc
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-18 15:48:44 -0500

    get workout routes

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
-const Workout = require("../../models/Workout");
+const db = require("../../models");
 
+router.get("/", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            let total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 module.exports = router;
