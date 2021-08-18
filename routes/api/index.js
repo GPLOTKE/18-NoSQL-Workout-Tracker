bacf09d7c15a6ac01472e67e7ad9f23ea67e093f
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-18 15:29:59 -0500

    nodemon

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const workoutRoutes = require('./workoutRoutes')
+const workoutRoutes = require('./workoutRoutes');
 
 router.use('/workouts', workoutRoutes);
 
